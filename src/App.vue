892a8eccae4a06f01784a7589a3798d61bc1a9bd
Michał Skwara <michalon2000@gmail.com>
2019-11-25 09:52:25 +0100

    Reactive buttons

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ title }}</h2>
+    <h3>{{ happiness }}</h3>
+    <br>
+    <button @click="add()">Add!</button>
+    <br>
+    <button @click="subtract()">Subtract!</button>
+    <br>
+    <button @click="startOver()">Start over!</button>
+    <br>
+    <button @click="changeHappy()">Happy or sad?</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    data() {
+      return {
+        title: 'Clicked 0 times!',
+        happiness: 'Jak sie czujesz?',
+        isHappy: false,
+        a: 0
+      }
+    },
+    methods: {
+      add(){
+        this.a = this.a + 1;
+        this.title = 'Clicked ' + this.a + " times!";
+      },
+      startOver(){
+        this.a = 0;
+        this.title = 'Clicked ' + this.a + " times!";
+        this.isHappy = false;
+        this.happiness = 'Jak sie czujesz?'
+      },
+      subtract(){
+        this.a = this.a - 1;
+        this.title = 'Clicked ' + this.a + " times!";
+      },
+      changeHappy(){
+        if(this.isHappy == false){
+          this.happiness = 'Happy';
+          this.isHappy = true;
+        }
+        else if(this.isHappy == true){
+          this.happiness = 'Sad';
+          this.isHappy = false;
+        }
+      }
+    }
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button{
+    padding: 10px;
+    margin: 10px;
+    border: 1px solid blue;
+    border-radius: 5px;
+
+  }
 </style>
